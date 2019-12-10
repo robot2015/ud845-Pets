167334412a63a280f6cb93f192189acb7d527066
Robert Beckman <rbeckman@pacificbiosciences.com>
2019-12-10 10:28:08 -0800

    Revert to starting-point and migrate to Android X.

--- app/src/main/java/com/example/android/pets/CatalogActivity.java
+++ app/src/main/java/com/example/android/pets/CatalogActivity.java
@@ -17,8 +17,8 @@ package com.example.android.pets;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.v7.app.AppCompatActivity;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
